34b9a8bff0d15dd9f29ee29af3b921c72dbdadf0
joseph-benoy <josephbenoy03@gmail.com>
2022-01-28 10:41:33 +0530

    base navbar

---
+++ ui/components/navbar.jsx
@@ -0,0 +1,42 @@
+import { Container,Nav,Form,Button,NavDropdown,FormControl ,Navbar} from 'react-bootstrap';
+const NavbarTop = () => {
+    return ( 
+        <Navbar bg="light" expand="lg" sticky='top' >
+  <Container fluid>
+    <Navbar.Brand href="#">Pets shop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">About</Nav.Link>
+        <NavDropdown title="Products" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#action2">Services</Nav.Link>
+        <Nav.Link href="#action2">Contact</Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+     );
+}
+ 
+export default NavbarTop;
