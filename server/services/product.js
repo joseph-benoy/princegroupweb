8b4b19e18fafd416474e3ac66cfc7a07e489a75d
joseph-benoy <josephbenoy03@gmail.com>
2022-01-28 14:23:38 +0530

    get menu endpoint fixed

--- server/services/product.js
+++ server/services/product.js
@@ -20,12 +20,52 @@ export const listAllProducts = async()=>{
 }
 export const getMenu = async()=>{
     try{
-        let menu = [];
-        let data = await sequelize.query("SELECT PRODUCT_TYPE.NAME AS TYPE,SUB_CATEGORY.NAME AS SUB,CATEGORY.NAME AS CATEGORY,CATEGORY.ICON_PATH AS ICON FROM PRODUCT_TYPE,SUB_CATEGORY,CATEGORY WHERE PRODUCT_TYPE.SUBCAT_ID=SUB_CATEGORY.ID AND SUB_CATEGORY.CATEGORY_ID=CATEGORY.ID",{
+        var menu = [];
+        const data = await sequelize.query("SELECT PRODUCT_TYPE.NAME AS TYPE,SUB_CATEGORY.NAME AS SUB,CATEGORY.NAME AS CATEGORY,CATEGORY.ICON_PATH AS ICON FROM PRODUCT_TYPE,SUB_CATEGORY,CATEGORY WHERE PRODUCT_TYPE.SUBCAT_ID=SUB_CATEGORY.ID AND SUB_CATEGORY.CATEGORY_ID=CATEGORY.ID",{
             type: QueryTypes.SELECT
         })
-        console.log(data)
-        return data;
+        var cats= data.map((item)=>(
+            item.CATEGORY
+        ))
+        cats = [...new Set(cats)];
+        var subcats = [];
+        var subCount = 0;
+        for(let cat of cats){
+            let temp = []
+            for(let item of data){
+                if(item.CATEGORY===cat){
+                    temp.push(item.SUB);
+                }   
+            }
+            subcats[subCount] =[...new Set(temp)]
+            subCount++;
+        }
+        let sb = [];
+        for(let sub of subcats){
+            let slist = []
+            for(let s of sub){
+                let temp = [];
+                for(let item of data){
+                    if(item.SUB===s){
+                        temp.push(item.TYPE)
+                    }
+                }
+                let subCat = {
+                    sub:s,
+                    types:[...new Set(temp)]
+                }
+                slist.push(subCat);
+            }
+            sb.push(slist);
+        }
+        var i=0;
+        menu = sb.map((item)=>(
+            {
+                main:cats[i++],
+                sub:item
+            }
+        ))
+        return menu;
     }
     catch(e){
         return e;
