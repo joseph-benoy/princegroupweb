21c7ea173314716f7a3d2b09517402fb9523ebc5
joseph-benoy <josephbenoy03@gmail.com>
2022-01-29 01:57:53 +0530

    login ui added

--- ui/src/pages/admin/admin.jsx
+++ ui/src/pages/admin/admin.jsx
@@ -1,8 +1,12 @@
-import { Row,Container,Col,Navbar,Nav } from "react-bootstrap";
+import { useState } from "react";
+import { Row,Container,Col,Navbar,Nav,Form,Button } from "react-bootstrap";
 import {useNavigate} from "react-router-dom"
-
+import "./admin.css"
 const Admin = () => {
     const nav = useNavigate();
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [error,setError] = useState("");
     return ( 
         <>
                     <header>
@@ -11,8 +15,8 @@ const Admin = () => {
                 <Navbar.Brand href="#home">Pet shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link onClick={()=>nav("/home")}>Home</Nav.Link>
+                <Nav className="me-auto">
+                    <Nav.Link onClick={()=>nav("/home")} id="home-link-admin">Home</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -21,7 +25,21 @@ const Admin = () => {
         <Container fluid>
             <Row>
                 <Col>
-                    
+                <Form className="login-form">
+                    <h2>Login</h2>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="danger" type="submit">
+                        Sign In
+                    </Button>
+                </Form>
                 </Col>
             </Row>
         </Container>
