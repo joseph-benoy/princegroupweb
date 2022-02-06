8da980cbd4420924ac930cfc4ffb0866fb47d2c0
joseph-benoy <josephbenoy03@gmail.com>
2022-02-06 20:13:52 +0530

    added url config for navlist

--- ui/src/components/navList/navlist.jsx
+++ ui/src/components/navList/navlist.jsx
@@ -4,12 +4,17 @@ import { NavDropdown } from "react-bootstrap";
 import axios from 'axios';
 import "./navlist.css"
 import { DropdownSubmenu, NavDropdownMenu} from "react-bootstrap-submenu";
+import {useNavigate} from "react-router-dom"
 const NavList = () => {
     const [menu,setMenu] = useState([]);
     useEffect(async()=>{
         let res = await axios.get("/api/product/menu");
         setMenu(res.data);
     },[]);
+    const nav = useNavigate();
+    const changePage = (link)=>{
+        nav(link);
+    }
     return ( 
         <NavDropdownMenu title="Products" id="navbarScrollingDropdown">
             {
@@ -20,7 +25,7 @@ const NavList = () => {
                                 <DropdownSubmenu title={subCat.sub} className="dm">
                                     {
                                         subCat.types.map((type)=>(
-                                            <NavDropdown.Item className="dm" href={"/product/"+type}>{type}</NavDropdown.Item>
+                                            <NavDropdown.Item className="dm" onClick={()=>{changePage("/type/"+cat.main+"/"+type)}}>{type}</NavDropdown.Item>
                                         ))
                                     }
                                 </DropdownSubmenu>
--- ui/src/components/sideNav/sidenav.jsx
+++ ui/src/components/sideNav/sidenav.jsx
@@ -2,18 +2,23 @@ import { useState,useEffect } from "react";
 import { Nav } from "react-bootstrap";
 import axios from "axios";
 import "./sidenav.css"
+import {useNavigate} from "react-router-dom"
 const SideNav = () => {
     const [menu,setMenu] = useState([]);
     useEffect(async()=>{
         let res = await axios.get("/api/product/menu");
         setMenu(res.data);
     },[]);
+    const nav = useNavigate();
+    const changePage = (link)=>{
+        nav(link);
+    }
     return ( 
         <Nav defaultActiveKey="/home" className="flex-column sidebar">
             <h4>Menu</h4>
             {
                 menu.map((cat)=>(
-                    <Nav.Link href={cat.main}><img className="drop-icon" src={"static/category_icons/"+cat.icon}/>&nbsp;&nbsp;&nbsp;{cat.main}</Nav.Link>
+                    <Nav.Link onClick={()=>{changePage("/category/"+cat.main)}}><img className="drop-icon" src={"static/category_icons/"+cat.icon}/>&nbsp;&nbsp;&nbsp;{cat.main}</Nav.Link>
                 ))
             }
         </Nav>
