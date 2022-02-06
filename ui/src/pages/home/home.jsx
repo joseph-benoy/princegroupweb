c25eee14fd66aa51218d48deb7a4afac8d208457
joseph-benoy <josephbenoy03@gmail.com>
2022-02-07 03:37:21 +0530

    conditional rendering

--- ui/src/pages/home/home.jsx
+++ ui/src/pages/home/home.jsx
@@ -9,19 +9,35 @@ import Offer from "./offer/offer";
 import oimg1 from "../../images/offer/offer8.jpg"
 import Services from "./services/services";
 import Review from "./review/review";
+import { useState } from "react";
+import { useEffect } from "react";
 const Home = () => {
+    const [mob,setMob] = useState(false)
+    useEffect(()=>{
+        if(window.innerWidth<786){
+            setMob(true)
+        }
+    },[mob])
+
     return ( 
         <>
       <header>
         <Header/>
       </header>
+        {
+            mob?<Slider1/>:null
+        }
             <Container fluid className="home-content">
                 <Row>
                     <Col lg={2} className="sidenav-col">
                         <SideNav/>
                     </Col>
                     <Col lg={10}>
-                        <Slider1/>
+                        {
+                                    
+                                    
+                                        !mob?<Slider1/>:null
+                    }
                     </Col>
                 </Row>
                 <Row>
