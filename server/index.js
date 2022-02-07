595efd0bf4e138b34cc57d81f55c725866269c80
joseph-benoy <josephbenoy03@gmail.com>
2022-02-07 20:42:31 +0530

    server configuerd

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import { AdminRouter } from "./routes/admin.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import { SliderRouter } from "./routes/slider.js";
+import path from 'path'
+
 
 const app = express();
 app.use(express.urlencoded({
@@ -20,14 +22,20 @@ app.use("/api/category",CategoryRouter);
 app.use("/api/product",ProductRouter);
 app.use("/api/admin",AdminRouter);
 app.use("/api/slider",SliderRouter);
-app.get("/",(req,res)=>{
-    res.json({
-        status:"success"
-    })
-})
+
+app.use(express.static(path.join("build")))
+
+app.use((req, res, next) => {
+    res.sendFile(path.join( "build", "index.html"));
+  });
+
 app.use((err,req,res,next)=>{
     res.status(400).json({error:err});
 })
+
+
+
+
 app.listen(process.env.PORT,()=>{
     console.clear()
     console.log("Server started at "+process.env.PORT);
