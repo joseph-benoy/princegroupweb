9be410fc602a302d14f75e571f92f6c6a3fc8ffc
joseph-benoy <josephbenoy03@gmail.com>
2022-02-07 22:48:48 +0530

    changes

--- server/utils/db.js
+++ server/utils/db.js
@@ -5,7 +5,7 @@ import initModels from "../models/init-models.js";
 
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    "PETSHOP",
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
         host:process.env.DB_HOST,
