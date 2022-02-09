8dd3983d360dea66bbc5b2fa0a01d15f9e1ea78c
joseph-benoy <josephbenoy03@gmail.com>
2022-02-10 00:52:33 +0530

    fixed about

--- ui/src/pages/home/about/about.jsx
+++ ui/src/pages/home/about/about.jsx
@@ -3,6 +3,9 @@ import img1 from '../../../images/about/about1.jpg';
 import img2 from '../../../images/about/about2.jpg';
 import "./about.css"
 const About = () => {
+    /*
+                        <Button id="about-btn" variant='primary'>Show now</Button>
+*/
     return ( 
         <Container fluid className="about-wrapper">
             <Row>
@@ -13,7 +16,6 @@ const About = () => {
                     <h3>About Prince</h3>
                     <p>We offer quality products at low prices every day. We have been in this business for almost 20 years. We have been doing online shopping very confidently. Our customers like and trust us so much because of the quality of our site's products.
                     </p>
-                    <Button id="about-btn" variant='primary'>Show now</Button>
                 </Col>
 
             </Row>
