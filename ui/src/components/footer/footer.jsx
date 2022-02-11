3dd9a98b5ce16f5c9bb1c2b8795b5b2ee1316e5e
joseph-benoy <josephbenoy03@gmail.com>
2022-02-11 17:50:26 +0530

    fixed footer

--- ui/src/components/footer/footer.jsx
+++ ui/src/components/footer/footer.jsx
@@ -1,54 +1,73 @@
 import { Container, Row,Col } from "react-bootstrap";
-import {GeoAltFill,TelephoneFill,Facebook, Twitter, Youtube, Instagram, EnvelopeOpenFill} from 'react-bootstrap-icons';
+import {GeoAltFill,TelephoneFill,Facebook, Twitter, Youtube, Instagram, EnvelopeFill, PhoneFill, Mailbox} from 'react-bootstrap-icons';
 import img from '../../images/bg2.jpg';
 import logo from '../../images/logo1.png'
 import "./footer.css";
 import {Search} from 'react-bootstrap-icons';
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const nav = useNavigate();
     return ( 
         <footer className="footer-area">
+            <Container fluid>
+                <Row className="footer-row">
+                    <Col lg={4}>
+                        <img src={logo} alt="logo" id="footer-logo"/>
+                        <h4>Prince Group of Companies</h4>
+                        <h5>Offices</h5>
+                        <Container fluid className="address_wrap">
+                            <Row>
+                                <Col lg={6}  className="address_inner">
+                                    <p>
+                                    Bank Street, Rolla,
+                                    Burj Avenue Building
+                                    Sharjah –U.A. E
+                                    </p>
+                                </Col>
+                                <Col lg={6}  className="address_inner">
+                                    <p>
+                                    P.O. Box: 10111
+                                    Industrial Area - 13
+                                    Sharjah –U.A. E
+                                    </p>
+                                </Col>
+                            </Row>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3 col-md-6 col-sm-6">
-                        <div className="single-footer-widget">
-                            <a href="index.html" className="logo">
-                                <img src={logo} alt="logo"  />
-                            </a>
-                            <ul className="footer-contact-info">
-                                <li><span>Tech support:</span> <a href="tel:+91 5678965320">+91 5678965320</a></li>
-                                <li><span>Email:</span>&nbsp;marketing@princegroupofcompany.com</li>
-                                <li><span>Address:</span>  Dubai</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6 col-sm-6" id="infocol">
-                        <div className="single-footer-widget pl-4">
-                            <h3>Information</h3>
-                            <ul className="custom-links">
-                                <li><a href="#about">About Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-6 col-sm-6">
-                        <div className="single-footer-widget">
-                            <h3>Subscribe to our newsletter!</h3>
-                            <p>Sign up for our mailing list to get the latest updates news & offers.</p>
-                            <form className="newsletter-form" data-toggle="validator">
-                                <input type="email" className="input-newsletter" placeholder="Your email address" name="EMAIL" required autoComplete="off" />
-                                <button type="submit"><Search /></button>
-                                <div id="validator-newsletter" className="form-result"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="copyright-area">
-                <div className="container">
-                    <p>Copyright @2022 Prince Group.All rights reserved.</p>
-                </div>
-            </div>
+                        </Container>
+                        
+                    </Col>
+                    <Col lg={5} className="footer-contact">
+                        <h4>Contact us</h4>
+                            <Row>
+                                <Col lg={5}>
+                                    <p><TelephoneFill/>+971 –67048192</p>
+                                </Col>
+                                <Col lg={5}>
+                                    <p><PhoneFill/>+971 - 521307547</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p className="footer-mail"><EnvelopeFill/>marketing@princegroupofcomapny.com</p>
+                                </Col>
+                            </Row>
+                    </Col>
+                    <Col lg={3} className="footer-links">
+                        <h4>Quick links</h4>
+                        <a href="/#about" style={{color:"white",textDecoration:"none"}}>About Us</a><br/>
+                        <a onClick={()=>{nav("admin")}}>Login</a><br/>
+                        <a onClick={()=>{nav("contact")}}>Contact Us</a>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Container className="copyright">
+                            <p>Copyright @2022 Prince Group.All rights reserved.</p>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
 
 
         </footer>
