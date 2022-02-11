804e4edbbe842c41ca1ef2417a67e0cab2a3c968
joseph-benoy <josephbenoy03@gmail.com>
2022-02-11 11:07:10 +0530

    fixed about section

--- ui/src/pages/home/about/about.jsx
+++ ui/src/pages/home/about/about.jsx
@@ -14,7 +14,25 @@ const About = () => {
                 </Col>
                 <Col lg={10} className="about-text">
                     <h3>About Prince</h3>
-                    <p>We offer quality products at low prices every day. We have been in this business for almost 20 years. We have been doing online shopping very confidently. Our customers like and trust us so much because of the quality of our site's products.
+                    <p>
+                    Prince Group, we are with great commitment and
+                    passion we strive to make tasty animal feeds of the very
+                    best quality, without unnecessarily burdening nature.
+                    The health of your animal is our top priority. And with it
+                    the high quality of our recipes, ingredients and
+                    sustainable products.
+                    Our range of private label pet food is made entirely in-
+                    house at the highest possible quality. Together with our
+                    dedicated team, we are committed to making your pet
+                    food product a real success!
+                    We run our business focusing on the highest quality
+                    standards and with great personal commitment. Our
+                    customers are essential to us and we believe in long-term
+                    commitments.
+                    Quality, service, product innovation and sustainability;
+                    these are our essential focuses for our successful
+                    operations and co-operation with our customers.<br/>
+                    “We make a positive difference on your loved Pets”
                     </p>
                 </Col>
 
