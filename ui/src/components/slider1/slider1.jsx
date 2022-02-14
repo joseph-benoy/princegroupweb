16894d2d6db75b5963e54da7365d6ee16ff76408
joseph-benoy <josephbenoy03@gmail.com>
2022-02-14 22:51:12 +0530

    chnaged hero images

--- ui/src/components/slider1/slider1.jsx
+++ ui/src/components/slider1/slider1.jsx
@@ -18,11 +18,6 @@ const Slider1 = () => {
                 src={item}
                 alt="First slide"
               />
-                  <Carousel.Caption>
-      <h1 className="slider-h">The Best Quality Organic Food</h1>
-      <p className="slider-p">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      <Button variant="primary" className="slider-btn">Shop now</Button>
-    </Carousel.Caption>
             </Carousel.Item>
             ))
           }
